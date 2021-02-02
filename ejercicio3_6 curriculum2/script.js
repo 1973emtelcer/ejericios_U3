8107e8c9543f963affee9fba525bd063b0285965
1973emtelcer <elenacerezal@gmail.com>
2021-02-02 20:05:42 +0000

    commit

---
+++ ejercicio3_6 curriculum2/script.js
@@ -0,0 +1,6 @@
+// Al hacer click sobre el botón de Imprimir salta un alert
+document.querySelector(".imprimir").addEventListener("click", saveData);
+
+function saveData() {
+  alert('Se ha imprimido el curriculum');
+};
